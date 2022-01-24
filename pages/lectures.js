ed38b2003d26b1ef8242c450d7473ce99a38b0b3
Paritosh Kadam <priyatamkadam@gmail.com>
2022-01-24 10:39:53 +0530

    rec lecturs and tt update

--- pages/lectures.js
+++ pages/lectures.js
@@ -1,77 +1,73 @@
 import Card from "../components/content/cards";
+import styles from "../styles/lectures.module.css"
+import { useState } from "react";
 
 export default function RecLect() {
-    const reclectures = [
+    
+    const [btn, setBtn] = useState(true);
+
+    const [data, setData] = useState([]);
+
+    const btns = [
       {
-        key:1,
-        name: "Mat Sci and Eng",
-        id: "Sujit R",
-        section: "L1/T2/P3",
-        link: "https://classroom.google.com/u/0/w/MzgzNDY3Nzg1Nzc1/tc/Mzg0MDA3MDA2NTA2",
+        id: 1,
+        name: "2-1 Lectures",
       },
       {
-         key:2,
-        name: "Maths 3",
-        id: "K bhargava",
-        section: "L7",
-        link: "https://drive.google.com/drive/folders/1LfaJOgg9V0BhUosFdCBqsA_ms2uRZY7G?usp=sharing",
+        id: 2,
+        name: "2-2 lectures",
       },
-      {
-        key:3,
-        name: "POE",
-        id: "Bheemeshwar",
-        section: "L2",
-        link: "https://drive.google.com/file/d/1mPO0drzJlYmdWeS_UVlSar5F0sA7X7AI/view?usp=sharing",
-      },
-      {
-        key: 4,
-        name: "Thermodynamics",
-        id: "Deepak Nabapure",
-        section: "P3",
-        link: "https://classroom.google.com/u/0/c/MTMwOTI5MTg4NzYz",
-      },
-      {
-        key:5,
-        name: "EPS",
-        id: "Santosh sir",
-        section: "L1",
-        link: "https://drive.google.com/drive/folders/1UtyZzVGTb9uFCXflvVKW6sAJHj9Tp41l",
-      },
-      {
-         key:6,
-        name: "Thermodynamics",
-        id: "Satish Dubey",
-        section: "L1",
-        link: "https://cms.bits-hyderabad.ac.in/course/view.php?id=13989",
-      },
-      {
-         key:7,
-        name: "Fluid Mechanics",
-        id: "Pradha Venkata",
-        section: "L1",
-        link: "https://cms.bits-hyderabad.ac.in/mod/forum/discuss.php?d=26501",
-      },
-      {
-         key:8,
-        name: "Mech Of Solids",
-        id: "Pavan Kumar",
-        section: "L1",
-        link: "https://classroom.google.com/u/0/c/MzgxNTYxODY4ODk2",
-      },
-      {
-        key: 9,
-        name: "Maths-tuts",
-        id: "No clue man",
-        section: "meh",
-        link:"https://drive.google.com/drive/u/1/folders/13SdZz-KY69xFWqrqzyNd7ckB7bPqHvVq"
-      }
     ];
+
+    const handleBack = () => {
+      setBtn(true);
+    };
+
+    const fetchapi = async (id) => {
+      if (id == 1) {
+        const response = await fetch("/api/recordings/lect21");
+        const data1 = await response.json();
+        setBtn(false);
+        setData(data1);
+        console.log("test");
+      } else if (id == 2) {
+        const response = await fetch("/api/recordings/lect22");
+        const data2 = await response.json();
+        setBtn(false);
+        setData(data2);
+        console.log("test");
+      }
+    };
+    
 
     return (
       <>
-        {reclectures.map((data) => (
-          <Card key={data.key}  {...data} />
-        ))}
+        <div className={btn ? styles.header : styles.null}>Books</div>
+        <div className={styles.btnContainer}>
+          {btns.map((data) => {
+            return (
+              <>
+                <div
+                  className={btn ? styles.btn : styles.null}
+                  key={data.id}
+                  onClick={() => {
+                    fetchapi(data.id);
+                  }}
+                >
+                  {data.name}
+                </div>
+              </>
+            );
+          })}
+        </div>
+        <div className={btn ? styles.null : styles.cards}>
+          {data.map((data) => (
+            <Card key={data.key} {...data} />
+          ))}
+          <div className={styles.back} onClick={handleBack}>
+            Back
+          </div>
+        </div>
       </>
     );
 }
